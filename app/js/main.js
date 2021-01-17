1002ec62bfc5acc209554dcf562fd9671f4bc96e
ogurec-i-molok0 <a.manczurow@yandex.ru>
2021-01-17 11:47:23 +0300

    added author section

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,11 @@
 $(function(){
 
+    $('.product-slider__inner').slick({
+        dots: true,
+        arrows: false,
+        slidesToShow: 4,
+        slidesToScroll: 4,
+    });
+
+    var mixer = mixitup('.products__inner-box');
 });
